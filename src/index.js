c8decb6055c2ad4eff6cc85eefa077cb757f4c18
gauravjha0711 <gauravjha0711@gmail.com>
2025-09-29 01:28:57 +0530

    utils code added and app set

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
 import dotenv from "dotenv";
 import mongoose from "mongoose";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 dotenv.config({
     path: "./.env"
 });
-connectDB();
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT||8000,()=>{
+        console.log(`Server is connected at PORT ${process.env.PORT||8000}`);
+    })
+})
+.catch((error)=>{
+    console.error("Error in DB connection", error);
+});
 
 
 
