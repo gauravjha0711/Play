7c0b2d89f54e42b8f61e4232660fa5c2463dc93c
gauravjha0711 <gauravjha0711@gmail.com>
2025-09-29 23:40:48 +0530

    user model updated access token jwt

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -20,7 +20,7 @@ const userSchema = new mongoose.Schema(
             lowercase: true,
             trim: true,
         },
-        fullname:{
+        fullName:{
             type: String,
             required: true,
             trim: true,
@@ -57,4 +57,31 @@ userSchema.pre("save", async function(next) {
     this.password = await bcrypt.hash(this.password, 10);
     next();
 })
+
+userSchema.methods.isPasswordCorrect = async function(password){
+    return await bcrypt.compare(password, this.password);
+}
+
+userSchema.methods.genrateAccessToken = function(){
+    return jwt.sign(
+        {
+            _id : this._id,
+            username: this.username,
+            email: this.email,
+            fullName: this.fullname,
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {expiresIn: process.env.ACCESS_TOKEN_EXPIRY}
+    )
+}
+userSchema.methods.genrateRefreshToken = function(){
+    return jwt.sign(
+        {
+            _id : this._id,
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {expiresIn: process.env.REFRESH_TOKEN_EXPIRY}
+    )
+}
+
 export const User = mongoose.model("User", userSchema);
