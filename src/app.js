bdcab4070f4f7c04e5ab50f50664d363afbc96a2
gauravjha0711 <gauravjha0711@gmail.com>
2025-10-01 13:22:43 +0530

    user routes update

--- src/app.js
+++ src/app.js
@@ -13,4 +13,11 @@ app.use(express.urlencoded({ extended: true, limit: '16kb' }));
 app.use(express.static('public'));
 app.use(cookieParser());
 
+
+//routes import
+import userRoutes from './routes/user.routes.js';
+
+//routes
+app.use('/api/v1/user', userRoutes);
+
 export {app};
