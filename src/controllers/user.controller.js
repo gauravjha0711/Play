1338a45953b6b951fa6e45db8dee40baa311e557
gauravjha0711 <gauravjha0711@gmail.com>
2025-10-03 14:55:18 +0530

    user routes add upload avater

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,18 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
 
 const registerUser = asyncHandler ( async (req,res) => {
-    res.status(200).json({
-        message: "User registered successfully"
-    });
+    //get user details from frontend
+    //validate user details
+    //check if user already exists
+    //check for image and avatar
+    //upload image to cloudinary
+    //create user object and save to db
+    //remove password from user object before sending response
+    //check if user is created successfully
+    //respond to frontend
+
+    const {username,email,password,fullName} = req.body;
+    console.log("email:", email);
 });
 
 export {registerUser};
